5a37f631bd9ffe2ed29541b617ffe811025e6a73
Allyn L. Bottorff <allyn@b6f.net>
2026-02-27 23:01:47 -0600

    Get github token from the environment

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,11 @@ use std::process::Command;
 ///
 /// A simple tool to clone and sync all of the repositories from a single GitHub organization
 /// This tool will attempt to find an authentication token for GitHub from the following sources,
-/// stopping after the first valid source found:
+/// in order of decreasing precedence:
 /// `gh auth token` -> `GITHUB_TOKEN` -> `GITHUB_PAT`
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(author, version, about)]
-struct Cli {
+struct CliFlags {
     ///Path to the directory where all repositories will be cloned or fetched.
     #[arg(short, long)]
     path: String,
@@ -49,19 +49,74 @@ struct Cli {
 }
 
 fn main() {
-    let _cli = Cli::parse();
+    let cli_flags = CliFlags::parse();
 
-    println!("gorc!")
+    println!("gorc!");
+
+    let token = get_github_token(&cli_flags).expect("Unable to get GitHub token from the environment.");
+
+    dbg!(cli_flags);
+    dbg!(token);
 }
 
-fn get_github_token() -> Option<String> {
-    let token_env_var: Option<String> = match env::var("GITHUB_TOKEN") {
-        Ok(v) => Some(v),
-        Err(_e) => match env::var("GITHUB_PAT") {
-            Ok(v) => Some(v),
-            Err(_e) => None,
-        },
-    };
+/// Get GitHub token from the environment. Early return on successfully finding a token
+fn get_github_token(cli_flags: &CliFlags) -> Option<String> {
+    // Attempt to get the GitHub token from the gh cli tool
+    let output = Command::new("gh").args(["auth", "token"]).output();
+    match output {
+        Ok(token) => {
+            match String::from_utf8(token.stdout) {
+                Ok(token) => {
+                    //TODO(alb): Validate the token
+                    if token != "" {
+                        let token: String = token.trim().into();
+                        return Some(token);
+                    }
+                }
+                Err(e) => {
+                    if cli_flags.verbose {
+                        eprintln!("Error parsing gh auth token output: {e}");
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            if cli_flags.verbose {
+                eprintln!("Error executing gh auth token: {e}");
+            }
+        }
+    }
 
-    return token_env_var;
+    // Attempt to get the token from the GITHUB_TOKEN env var
+    match env::var("GITHUB_TOKEN") {
+        Ok(token) => {
+            //TODO(alb): Improve the token validation
+            let token: String = token.trim().into();
+            if token != "" {
+                return Some(token);
+            }
+        }
+        Err(e) => {
+            if cli_flags.verbose {
+                eprintln!("Error reading GITHUB_TOKEN env var: {e}")
+            }
+        }
+    }
+
+    // Attempt to get the token from the GITHUB_PAT env var
+    match env::var("GITHUB_PAT") {
+        Ok(token) => {
+            //TODO(alb): Improve the token validation
+            let token: String = token.trim().into();
+            if token != "" {
+                return Some(token);
+            }
+        }
+        Err(e) => {
+            if cli_flags.verbose {
+                eprintln!("Error reading GITHUB_PAT env var: {e}")
+            }
+        }
+    }
+    None
 }
